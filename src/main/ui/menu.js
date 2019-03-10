934c1b82f6ec4c19c6e51053df18ea0734176796
Jay Weeks <jay.patrick.weeks@gmail.com>
2019-03-10 14:39:33 -0700

    Enable copy, paste, undo, redo for input fields

--- src/main/ui/menu.js
+++ src/main/ui/menu.js
@@ -36,7 +36,18 @@ export function createMenuTemplate (app, actions) {
         }
       ]
     },
-
+    {
+      label: 'Edit',
+      submenu: [
+        { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+        { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+        { type: 'separator' },
+        { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+        { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+        { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+        { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
+      ]
+    },
     {
       label: 'Window',
       submenu: [
@@ -63,7 +74,7 @@ export function createMenuTemplate (app, actions) {
     })
 
     // Window menu
-    template[2].submenu = [
+    template[3].submenu = [
       {role: 'close'},
       {role: 'minimize'},
       {role: 'zoom'},
